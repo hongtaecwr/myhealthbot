551be0b187a854f11ac2c44df60dc93e1824a73a
hongtaecwr <t.chawarat@kkumail.com>
2018-11-12 22:39:23 +0700

    v10

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-const server = require('express');
-const PORT = process.env.port || 9999;
-const request = require('request');
-const bodyParser = require('body-parser');
+const
+    bodyParser = require('body-parser'),    
+    express = require('express'),
+    line = require('@line/bot-sdk'),
+    config = require('./config.json');
 
-server()
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false}))
-    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+    require('dotenv').config();
+
+const app = express();
+
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    res.sendStatus(200)
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+
+});
+
+function handleEvent(event) {
+
+    console.log(event);
+    if (event.type === 'message' && event.message.type === 'text') {
+        handleMessageEvent(event);
+    } else {
+        return Promise.resolve(null);
+    }
+}
+
+function handleMessageEvent(event) {
+    var msg = {
+        type: 'text',
+        text: 'สวัสดีครัช'
+    };
+
+    return client.replyMessage(event.replyToken, msg);
+}
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+    console.log('run at port', app.get('port'));
+});
