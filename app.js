4541c07c889878c22759fbc8a35cea6e1cf87fcd
hongtaecwr <t.chawarat@kkumail.com>
2018-11-12 21:56:55 +0700

    v2

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+const server = require('express');
+const PORT = process.env.PORT || 9999;
+const request = require('request');
+const bodyParser = require('body-parser');
+
+server()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false}))
+    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
